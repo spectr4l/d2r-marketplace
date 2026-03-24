6a15145bec59ac7cf5fa8aad16eaa2ecbc8da7e3
spectr0h <willhubner@yahoo.com.br>
2026-03-24 10:36:28 -0300

    Upload save game test

---
+++ modules/d2_writer.py
@@ -0,0 +1,8 @@
+def write_item_to_shared_stash(stash_path: str, item: dict) -> None:
+    """
+    1. abre o .d2i
+    2. insere o item em memória
+    3. reserializa o stash
+    4. grava de volta
+    """
+    raise NotImplementedError
